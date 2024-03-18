54a462a0f91f5b7fcc64cf622ddee029db2d6d2a
Icaro Ramos <polosnramos@hotmail.com>
2024-03-18 11:45:25 -0300

    feat: Update styles for Quiz screen

--- src/screens/Quiz/styles.ts
+++ src/screens/Quiz/styles.ts
@@ -1,19 +1,29 @@
-import { StyleSheet } from 'react-native';
-import { THEME } from '../../styles/theme';
+import { StyleSheet } from "react-native";
+import { THEME } from "../../styles/theme";
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: THEME.COLORS.GREY_800,
   },
+  header: {
+    width: "100%",
+  },
   question: {
-    alignItems: 'center',
+    alignItems: "center",
     paddingTop: 80,
     paddingBottom: 300,
     padding: 32,
   },
+  title: {
+    fontFamily: THEME.FONTS.BOLD,
+    color: THEME.COLORS.GREY_100,
+    fontSize: 16,
+    marginBottom: 7,
+    textAlign: "center",
+  },
   footer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginTop: 24,
-  }
+  },
 });
